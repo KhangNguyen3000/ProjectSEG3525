02faa327bddd43c7bea9f41a16a58930a90ef9f2
Adam Lawson <alaws057@uottawa.ca>
2019-05-30 14:38:53 -0400

    added event listeners to each card that redirect to the menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
- 
+window.onload = function() {
+    var elements = document.getElementsByClassName("card");
+    var numElements = elements.length;
+    for (i = 0; i <numElements; i++) {
+        elements[i].addEventListener("click", redirect);
+    }
+}
+function redirect() {
+        window.location.href = 'menu.html';}
