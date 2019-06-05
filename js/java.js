9831b4e36e54ad4c9aebe3752c9c78e9f6c6fa1c
KhangNguyen3000 <nguyenh.khang.1999@gmail.com>
2019-06-05 00:33:36 -0400

    done

--- js/java.js
+++ js/java.js
@@ -2,17 +2,14 @@ var inames = [];
 var iprice = [];
 
 
-
+/*exported displayCart*/
 function displayCart() {
-
-    var cartdata = '<ul class="card2">';
-    var total = 0;
+   var cartdata = "";
     for(var i = 0; i < inames.length; i++){
-        total = total + iprice[i];
-        cartdata = cartdata + '<div>' + '<label>' + inames[i] + '  </label>'+ '<label>$' + iprice[i] + '</label></div><label>----------</label>'
+        if(inames[i] != ","){
+        cartdata = cartdata + '<div class="pad2"><label>'+inames[i]+'</label><label>...........</label><label> $'+iprice[i]+'</label><button onclick="removeI2(\''+inames[i]+'\')" class="boxed br" type="button" style="float:right"> Cancel </button><br></div><br><br>';
+        }
     }
-    cartdata = cartdata + '</ul>'
-
     document.getElementById('cart').innerHTML = cartdata;
 }
 
@@ -23,4 +20,69 @@ function addItem(iden, price) {
     inames.push(iden);
     iprice.push(price);
     displayCart();
+}
+/* exported checkout */
+/*eslint-env browser*/
+function checkout(){
+    sessionStorage.clear();
+    sessionStorage.setItem("inames", JSON.stringify(inames));
+   sessionStorage.setItem("iprice", JSON.stringify(iprice));
+}
+/* exported loadCart */
+/*eslint-env browser*/
+function loadCart(){
+    inames = JSON.parse(sessionStorage.getItem("inames"));
+    iprice = JSON.parse(sessionStorage.getItem("iprice"));
+    showCart();
+}
+/* exported loadCart2 */
+/*eslint-env browser*/
+function loadCart2(){
+    inames = JSON.parse(sessionStorage.getItem("inames"));
+    iprice = JSON.parse(sessionStorage.getItem("iprice"));
+    displayCart();
+}
+
+function showCart(){
+    var cartdata = '<label class="b fon">command:</label><br>';
+    for(var i = 0; i < inames.length; i++){
+        if(inames[i] != ","){
+        cartdata = cartdata + '<div class="pad2"><label>'+inames[i]+'</label><label>...........</label><label> $'+iprice[i]+'</label><button onclick="removeI(\''+inames[i]+'\')" class="boxed br" type="button" style="float:right"> Cancel </button><br></div>';
+        }
+    }
+    document.getElementById('cart').innerHTML = cartdata;
+}
+/* exported removeI */
+/*eslint-env browser*/
+function removeI(iname){
+    for(var i = 0; i < inames.length; i++){
+        if(iname == inames[i]){
+            inames.splice(i,1);
+            iprice.splice(i,1);
+            break;
+           }
+    }
+    showCart();
+}
+/* exported removeI2 */
+/*eslint-env browser*/
+function removeI2(iname){
+    for(var i = 0; i < inames.length; i++){
+        if(iname == inames[i]){
+            inames.splice(i,1);
+            iprice.splice(i,1);
+            break;
+           }
+    }
+    displayCart();
+}
+
+/* exported first */
+/*eslint-env browser*/
+function first(){
+    if(sessionStorage.length < 1){
+         document.getElementById('cart').innerHTML = "Begin adding items to your order and they will show up here";
+    }else{
+        loadCart2();
+    }
 }
