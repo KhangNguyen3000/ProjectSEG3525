46323e191ab9742833885c5cf59ae46aff80a825
Adam Lawson <alaws057@uottawa.ca>
2019-06-05 12:27:09 -0400

    added input validation and an order confirmation message

--- js/confirmationpage.js
+++ js/confirmationpage.js
@@ -40,3 +40,26 @@ window.onload = function(){
 
 }
 
+function prev() {
+    sessionStorage.setItem("items",names);
+    sessionStorage.setItem("prices",iprices);
+    window.location.href = 'pagedeCommande.html';
+}
+
+function validateForm(){
+    var fname = document.forms["cc-form"]["fname"].value;
+    var lname = document.forms["cc-form"]["lname"].value;
+    var cnum = document.forms["cc-form"]["cnum"].value;
+    var cvv = document.forms["cc-form"]["cvv"].value;
+    var ccexp = document.forms["cc-form"]["ccexp"].value;
+    (fname == "") ? document.getElementById("fname").innerHTML = "Must not be Empty": $("#fname").hide();
+    (lname == "") ? document.getElementById("lname").innerHTML = "Must not be Empty": $("#lname").hide();
+    (cnum == "") ? document.getElementById("cnum").innerHTML = "Must not be Empty": $("#cnum").hide();
+    (cvv == "") ? document.getElementById("cvv").innerHTML = "Must not be Empty": $("#cvv").hide();
+    (ccexp == "") ? document.getElementById("ccexp").innerHTML = "Must not be Empty": $("#ccexp").hide();
+    if(!(fname==""||lname==""||cnum==""||cvv==""||ccexp=="")){
+        console.log("here");
+        $('#order-confirm').modal('show');
+    }
+}
+
